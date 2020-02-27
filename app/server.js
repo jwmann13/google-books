82d7241cad14e2473af5047ee71613183ea35393
Jeffrey Mann <jwmann22@gmail.com>
2020-02-26 20:21:55 -0500

    created get and post methods for books from db

--- app/server.js
+++ app/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const path = require("path");
 const PORT = process.env.PORT || 6200;
 const app = express();
+
+const session = require("express-session");
+const { dbConnection } = require("./models");
+const MongoStore = require("connect-mongo")(session);
+
+const routes = require("./routes");
+
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,13 +20,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("public"));
 }
 
-// Define API routes here
+app.use(session({
+  secret: "cor-blimey",
+  store: new MongoStore({ mongooseConnection: dbConnection }),
+  resave: false,
+  saveUninitialized: false
+}));
 
-// Send every other request to the React app
-// Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.json({});
-// });
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
