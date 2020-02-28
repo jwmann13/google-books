1665eb09c4cfc5b03736ea6d287f3d2ab6180b04
Jeffrey Mann <jwmann22@gmail.com>
2020-02-28 14:21:15 -0500

    moved searchbar inside book container to call to new google api search and update listed books

--- client/src/components/BookContainer/index.js
+++ client/src/components/BookContainer/index.js
@@ -1,33 +1,31 @@
 import React, { useState } from "react";
-import API from "../../utils/API";
 
-import { Book } from "../index";
+import { Book, SearchBar } from "../index";
 
 function BookContainer() {
   const [books, setBooks] = useState([]);
 
-  API.getBooks()
-    .then(response => {
-      setBooks(response.data)
-    })
-    .catch(err => console.log(err));
-
-  const bookCards = books.map((b, i) => {
-    if (i % 5 === 4) {
-      return (
-        <>
-          <Book>{b}</Book>
-          <div className="w-100"></div>
-        </>
-      );
-    } else {
-      return <Book>{b}</Book>;
-    }
-  });
+  let bookCards = [];
+  if (books !== []) {
+    bookCards = books.map((b, i) => {
+      if (i % 3 === 2) {
+        return (
+          <>
+            <Book>{b}</Book>
+            <div className="w-100"></div>
+          </>
+        );
+      } else {
+        return <Book>{b}</Book>;
+      }
+    });
+  } else {
+    bookCards = []
+  }
 
   return (
     <div className="container">
-      <div className="row"></div>
+      <div className="row"><SearchBar setBooks={setBooks}/></div>
       <div className="row">{bookCards}</div>
     </div>
   );
--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SearchBar() {
-    return (
-        <form action="" method="get">
-            <input type="text" name="search" id="search" />
-            <input type="submit" value="Search" className="btn btn-primary"/>
-        </form>
-    );
+import API from "../../utils/API";
+
+function SearchBar(props) {
+  const [q, setQ] = useState("");
+
+  function getSearch(e) {
+    e.preventDefault();
+    API.getSearch({ q })
+      .then(response => {
+        props.setBooks(response.data);
+      })
+      .catch(err => console.log(err));
+  }
+
+  function changeQ(e) {
+    setQ(e.target.value);
+  }
+
+  return (
+    <form onSubmit={getSearch} className="form-inline mb-3">
+      <input
+        type="text"
+        name="q"
+        id="q"
+        className="form-control w-30"
+        onChange={changeQ}
+      />
+      <input type="submit" value="Search" className="btn btn-primary ml-2" />
+    </form>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
