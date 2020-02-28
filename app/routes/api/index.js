b8ebc46e8cbd3b08306ac02324921284829c2b27
Jeffrey Mann <jwmann22@gmail.com>
2020-02-28 14:16:39 -0500

    added api route to search from google books

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const books = require("./books");
+const search = require("./search");
 
 router.use("/books", books);
+router.use("/search", search);
 
 module.exports = router;
