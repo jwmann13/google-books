a2da3e72349486ee5d5091b629d53daf3960a83a
Jeffrey Mann <jwmann22@gmail.com>
2020-03-13 13:06:26 -0400

    created user context consumer and provider on app.js and api utils to interact with backend to authenticate user

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,96 @@
-import React, { Component } from "react";
+import React, { Component, createContext } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { Saved, Search, Register } from "./pages";
+import { Saved, Search, Register, Login } from "./pages";
+import { AccountDropdown } from "./components";
+import API from "./utils/API";
 import "./App.css";
 
+const UserContext = createContext();
+export const UserConsumer = UserContext.Consumer;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.login = (e, credentials) => {
+      e.preventDefault();
+      API.login(credentials)
+        .then(response => this.setState({ user: response.data }))
+        .catch(err => console.log(err));
+    };
+
+    this.logout = e => {
+      e.preventDefault();
+      API.logout()
+        .then(() => this.setState({ user: null }))
+        .catch(err => console.log(err));
+    };
+
+    this.state = {
+      user: null,
+      login: this.login,
+      logout: this.logout
+    };
+
+    this.login.bind(this);
+    this.logout.bind(this);
+  }
+
+  componentDidMount() {
+    API.currentUser().then(response => this.setState({ user: response.data }));
+  }
+
   render() {
     return (
-      <Router>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link to="/" className="navbar-brand">
-            Navbar
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+      <UserContext.Provider value={this.state}>
+        <Router>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">
+              Navbar
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <Link to="/saved" className="nav-link">
-                  Saved <span className="sr-only">(current)</span>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/register" className="nav-link">
-                  Register
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to="/saved" className="nav-link">
+                    Saved <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <AccountDropdown />
+              </ul>
+            </div>
+          </nav>
 
-        <Switch>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/saved">
-            <Saved />
-          </Route>
-          <Route path="/">
-            <Search />
-          </Route>
-        </Switch>
-      </Router>
+          <Switch>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/saved">
+              <Saved />
+            </Route>
+            <Route path="/">
+              <Search />
+            </Route>
+          </Switch>
+        </Router>
+      </UserContext.Provider>
     );
   }
 }
